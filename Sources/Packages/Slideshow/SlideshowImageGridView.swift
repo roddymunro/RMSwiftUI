6b6af5d3e537e623f68a7a606947385dd1cfa35a
Roddy Munro <roddymunro@icloud.com>
2021-07-13 19:44:36 -0300

    Another temp fix

--- Sources/Packages/Slideshow/SlideshowImageGridView.swift
+++ Sources/Packages/Slideshow/SlideshowImageGridView.swift
@@ -44,7 +44,7 @@ public struct SlideshowImageGridView: View {
             LazyVGrid(columns: Array(repeating: .init(.flexible()), count: imagesPerRow), alignment: .leading, spacing: 8) {
                 ForEach(images.indices, id: \.self) { index in
                     if index < images.count {
-                        ZStack(alignment: .topTrailing) {
+//                        ZStack(alignment: .topTrailing) {
                             Button(action: {
                                 withAnimation(.easeInOut) {
                                     openImage(at: index)
@@ -53,20 +53,20 @@ public struct SlideshowImageGridView: View {
                                 SlideshowImageGridItem(images: $images, index: index, imageHeight: imageHeight, maximumImagesToShow: maximumImagesToShow, cornerRadius: cornerRadius)
                             }
                             
-                            if let maximumImagesToShow = maximumImagesToShow, images.count > maximumImagesToShow && index == maximumImagesToShow - 1 {
-                                // show no button
-                            } else if let onDeleteButtonTapped = onDeleteButtonTapped {
-                                Button(action: { onDeleteButtonTapped(index) }) {
-                                    Image(systemName: "trash")
-                                        .font(.system(size: 16, weight: .bold))
-                                        .foregroundColor(.white)
-                                        .padding(8)
-                                        .background(Color.black)
-                                        .clipShape(Circle())
-                                }
-                                .padding(6)
-                            }
-                        }
+//                            if let maximumImagesToShow = maximumImagesToShow, images.count > maximumImagesToShow && index == maximumImagesToShow - 1 {
+//                                // show no button
+//                            } else if let onDeleteButtonTapped = onDeleteButtonTapped {
+//                                Button(action: { onDeleteButtonTapped(index) }) {
+//                                    Image(systemName: "trash")
+//                                        .font(.system(size: 16, weight: .bold))
+//                                        .foregroundColor(.white)
+//                                        .padding(8)
+//                                        .background(Color.black)
+//                                        .clipShape(Circle())
+//                                }
+//                                .padding(6)
+//                            }
+//                        }
                     }
                 }
                 if let onAddButtonTapped = onAddButtonTapped {
